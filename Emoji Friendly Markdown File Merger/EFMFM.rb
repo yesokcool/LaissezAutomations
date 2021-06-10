c385dab8d0a127f71d62142880074250bb092f26
OK <63081353+yesokcool@users.noreply.github.com>
2021-06-10 01:23:14 -0400

    Add files via upload

--- Emoji Friendly Markdown File Merger/EFMFM.rb
+++ Emoji Friendly Markdown File Merger/EFMFM.rb
@@ -5,10 +5,19 @@ if ARGV.length < 1
 	exit
 end
 
+
+if (ARGV.length == 2)
+	separator = ARGV[1]
+else
+	separator = ""
+end
+
 folder_path = ARGV[0] + "\\"
+
+File.write(".\\EFMFM_merged_file.md", "", mode:'w')
 
 Dir.foreach(folder_path, encoding: Encoding::UTF_8) do |filename|
 # Skip current and parent directories.
 	next if filename == '.' or filename == '..'
-	File.write(".\\EFMFM_merged_file.md", (File.read(folder_path + filename) + "\n"), mode: 'a')
+	File.write(".\\EFMFM_merged_file.md", (File.read(folder_path + filename) + "\n\n" + separator + "\n\n"), mode: 'a')
 end
